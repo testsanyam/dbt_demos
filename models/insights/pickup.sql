b947574cb2e611336db176c5cd3a15e5db920263
Sanyam Goel <sanyam.goel@matillion.com>
2024-01-23 09:07:31 +0000

    corrected pickup file to previous settings

--- models/insights/pickup.sql
+++ models/insights/pickup.sql
@@ -1,5 +1,5 @@
 SELECT "TaxiZone_pickup", COUNT("TaxiZone_pickup") AS Common_Pickup
-FROM {{ ref('stg_yellow_taxi') }}
+FROM SANYAM_GOEL.DB_DWH."dmn_yellow"
 GROUP BY "TaxiZone_pickup"
 ORDER BY Common_Pickup DESC
 LIMIT 1
